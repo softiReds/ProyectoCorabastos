c6b90314455f7e31aa4333962ce602939bacb5b4
Ximena Delgado <xim.delgado@outlook.com>
2024-11-16 11:50:41 -0500

    Nueva clase Producto

---
+++ Backend/Models/Producto.cs
@@ -0,0 +1,9 @@
+﻿namespace CorabastosAPI.Models;
+
+public class Producto
+{
+    public Guid ProductoId { get; set; }
+    public string ProductoNombre { get; set; }
+    public int ProductoCantidad { get; set; }
+    public int ProductoPrecio { get; set; }
+}
