04b620a7650c2648d454cb4fc77802db198ec5ac
Santiago Rojas <santiagor@parserr.com>
2024-11-16 10:03:52 -0500

    Se crean modelos Usuario, TipoUsuario y Ciudad con sus respectivas relaciones

---
+++ Backend/Models/TipoUsuario.cs
@@ -0,0 +1,9 @@
+﻿namespace CorabastosAPI.Models;
+
+public class TipoUsuario
+{
+    public Guid TipoUsuarioId { get; set; }
+    public string TipoUsuarioDescripcion { get; set; }
+    
+    public virtual ICollection<Usuario> Usuario { get; set; }
+}
