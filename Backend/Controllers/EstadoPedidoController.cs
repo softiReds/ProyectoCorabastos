0857e6d180dd26fadd19a1b6f2cfed324d5e11ab
Mateo Villamizar <matiusvil234@gmail.com>
2024-11-27 00:14:26 -0500

    Creación de los controllers de Pedido, EstadoPedido y TipoUsuario.

---
+++ Backend/Controllers/EstadoPedidoController.cs
@@ -0,0 +1,49 @@
+﻿using CorabastosAPI.Models;
+using CorabastosAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorabastosAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EstadoPedidoController : ControllerBase
+{
+    private readonly IEstadoPedidoService _estadoPedidoService;
+
+    public EstadoPedidoController(IEstadoPedidoService estadoPedidoService) => _estadoPedidoService = estadoPedidoService;
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var estadoPedido = await _estadoPedidoService.Get();
+        return Ok(estadoPedido);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
+    {
+        var estadoPedido = await _estadoPedidoService.GetById(id);
+        return Ok(estadoPedido);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] EstadoPedido estadoPedido)
+    {
+        await _estadoPedidoService.Post(estadoPedido);
+        return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult Put([FromBody] EstadoPedido estadoPedido)
+    {
+        _estadoPedidoService.Put(estadoPedido);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete([FromRoute] Guid id)
+    {
+        _estadoPedidoService.Delete(id);
+        return Ok();
+    }
+}
