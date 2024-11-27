a746d240fb7be8936355a5953b2ffa87ca4f5ef3
Ximena Delgado <xim.delgado@outlook.com>
2024-11-26 22:56:14 -0500

    Creación de interfaces y servicios: CarritoCompras, CarritoComprasProducto, Producto.

---
+++ Backend/Services/IProductoService.cs
@@ -0,0 +1,12 @@
+﻿using CorabastosAPI.Models;
+
+namespace CorabastosAPI.Services;
+
+public interface IProductoService
+{
+    Task<List<Producto>> Get();
+    Task<Producto> GetById(Guid id);
+    Task Post(Producto producto);
+    void Put(Producto producto);
+    void Delete(Guid id);
+}
---
+++ Backend/Services/ProductoService.cs
@@ -0,0 +1,24 @@
+﻿using CorabastosAPI.Models;
+using CorabastosAPI.Repositories;
+
+namespace CorabastosAPI.Services;
+
+public class ProductoService : IProductoService
+{
+    private readonly IRepository<Producto> _productoRepository;
+
+    public ProductoService(IRepository<Producto> productoRepository)
+    {
+        _productoRepository = productoRepository;
+    }
+
+    public Task<List<Producto>> Get() => _productoRepository.Get();
+
+    public Task<Producto> GetById(Guid id) => _productoRepository.GetById(id);
+
+    public Task Post(Producto producto) => _productoRepository.Create(producto);
+
+    public void Put(Producto producto) => _productoRepository.Update(producto);
+
+    public void Delete(Guid id) => _productoRepository.Delete(id);
+}
