c0dedee384c9d1b2dc16a6178637a05e6829bb44
Ximena Delgado <xim.delgado@outlook.com>
2024-11-26 21:15:35 -0500

    Creación de repositorios: CarritoComprasProductoRepository.cs, CarritoComprasRepository.cs y ProductoRepository.cs

---
+++ Backend/Repository/CarritoComprasProductoRepository.cs
@@ -0,0 +1,45 @@
+﻿using CorabastosAPI.Context;
+using CorabastosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorabastosAPI.Repositories;
+
+public class CarritoComprasProductoRepository : IRepository<CarritoComprasProducto>
+{
+    private CorabastosContext _dbContext;
+
+    public CarritoComprasProductoRepository(CorabastosContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<List<CarritoComprasProducto>> Get() => await _dbContext.CarritoComprasProductos.ToListAsync();
+    public Task<CarritoComprasProducto> GetById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CarritoComprasProducto> GetById(Guid carritoComprasId, Guid productoId) 
+        => await _dbContext.CarritoComprasProductos.FindAsync(carritoComprasId, productoId);
+
+    public async Task Create(CarritoComprasProducto entity) => await _dbContext.CarritoComprasProductos.AddAsync(entity);
+
+    public void Update(CarritoComprasProducto entity)
+    {
+        _dbContext.CarritoComprasProductos.Attach(entity);
+        _dbContext.CarritoComprasProductos.Entry(entity).State = EntityState.Modified;
+    }
+
+    public void Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // public async void Delete(Guid carritoComprasId, Guid productoId)
+    // {
+    //     var carritoComprasProducto = await GetById(carritoComprasId, productoId);
+    //     _dbContext.CarritoComprasProductos.Remove(carritoComprasProducto);
+    // }
+    
+    public async Task SaveChanges() => await _dbContext.SaveChangesAsync();
+}
---
+++ Backend/Repository/CarritoComprasRepository.cs
@@ -0,0 +1,35 @@
+﻿using CorabastosAPI.Context;
+using CorabastosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorabastosAPI.Repositories;
+
+public class CarritoComprasRepository : IRepository<CarritoCompras>
+{
+    private CorabastosContext _dbContext;
+
+    public CarritoComprasRepository(CorabastosContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<List<CarritoCompras>> Get() => await _dbContext.CarritosCompras.ToListAsync();
+
+    public async Task<CarritoCompras> GetById(Guid id) => await _dbContext.CarritosCompras.FindAsync(id);
+
+    public async Task Create(CarritoCompras entity) => await _dbContext.CarritosCompras.AddAsync(entity);
+
+    public void Update(CarritoCompras entity)
+    {
+        _dbContext.CarritosCompras.Attach(entity);
+        _dbContext.CarritosCompras.Entry(entity).State = EntityState.Modified;
+    }
+
+    public async void Delete(Guid id)
+    {
+        var carritoCompras = await GetById(id);
+        _dbContext.CarritosCompras.Remove(carritoCompras);
+    }
+
+    public async Task SaveChanges() => await _dbContext.SaveChangesAsync();
+}
